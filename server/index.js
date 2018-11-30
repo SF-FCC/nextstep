069bdfcea1bdb84031cfa055d0838e73be51e948
Christian Zenaty <cazenaty@yahoo.com>
2018-11-30 11:10:08 -0800

    Adding morgan and middleware setup

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
 
 app.get('/',  (req, res) => {
     res.send('getting started...');
 });
 
 //Server Setup
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Serving starting on ${PORT}`);
 })

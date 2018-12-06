915e6c6a6f0ba76d4d342aafe2db84ac35f6e98d
Tiffany Lam <tlam5@sbcglobal.net>
2018-12-06 14:40:09 -0800

    Include started action creators and reducers

---
+++ client/src/actions/index.js
@@ -0,0 +1,66 @@
+export const register = userInfo => {
+  return {
+    type: 'REGISTER',
+    payload: userInfo
+  }
+}
+
+export const login = userInfo => {
+  return {
+    type: 'LOGIN',
+    payload: userInfo
+  }
+}
+
+export const logout = () => {
+  return {
+    type: 'LOGOUT'  }
+}
+
+export const updateEmail = email => {
+  return {
+    type: 'UPDATE_EMAIL',
+    payload: email
+  }
+}
+
+export const updatePassword = password => {
+  return {
+    type: 'UPDATE_PASSWORD',
+    payload: password
+  }
+}
+
+export const deleteAccount = userInfo => {
+  return {
+    type: 'DELETE_ACCOUNT',
+    payload: userInfo
+  }
+}
+
+export const getJobApps = user => {
+  return {
+    type: 'GET_JOB_APPS',
+    // payload: user.jobApps ?
+  }
+}
+
+export const addJobApp = details => {
+  return {
+    type: 'ADD_JOB_APP',
+    payload: details
+  }
+}
+
+export const updateJobApp = id => {
+  return {
+    type: 'UPDATE_JOB_APP',
+    // payload: details
+  }
+}
+
+export const deleteJobApp = id => {
+  return {
+    type: 'DELETE_JOB_APP'
+  }
+}

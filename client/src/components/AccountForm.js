59e6a07bf79a66d2006ee44cfcf3b5862bd224dc
Nate Lipp <Nateplipp@gmail.com>
2018-12-07 13:08:53 -0800

    add browserrouter & prop-types lib & acc dropdown
    * feature/react-routing-routes
    
    * feature/dashboard-account-dropdown
    
    * create account form and routing, add prop-types library
    
    * Add Switch component wrapping main routes
    
    this ensures only one of the child components will render

---
+++ client/src/components/AccountForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AccountForm extends Component {
+  render() {
+    return (
+      <div>form goes here</div>
+    )
+  }
+}
+
+export default AccountForm;
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
+
+import Tracker from './components/Tracker';
+import Account from './components/Account';
+import Dashboard from './components/Dashboard';
+import AccountForm from './components/AccountForm';
  
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App>
+      <Switch>
+        <Route path="/tracker" component={Tracker} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/account/form" component={AccountForm} />
+      </Switch>
+    </App>
+  </BrowserRouter>, 
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

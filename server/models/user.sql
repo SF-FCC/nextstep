b9a7e6632ee808cbd1da44ea665280a4759a9465
Christian Zenaty <cazenaty@yahoo.com>
2018-12-06 16:48:19 -0800

    Add user model

---
+++ server/models/user.sql
@@ -0,0 +1,11 @@
+BEGIN TRANSACTION;
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+COMMIT;

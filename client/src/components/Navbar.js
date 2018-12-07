f8feff492ea45ce17632e9b518ee2677132d7fca
Nalipp <nateplipp@gmail.com>
2018-12-07 10:54:28 -0800

    feature/dashboard-account-dropdown

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Account from './Account';
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showAccountDropdown: false,
+    }
+    this.toggleAccountDropdown = this.toggleAccountDropdown.bind(this);
+  }
+  toggleAccountDropdown() {
+    this.setState({ showAccountDropdown: !this.state.showAccountDropdown });
+  }
   render() {
     return (
       <div>
         <h1>NextStep</h1>
         <Link to="/dashboard">Dashboard</Link>
         <Link to="/tracker">Tracker</Link>
-        <Link to="/account">Account</Link>
+        <div>
+          <p onClick={this.toggleAccountDropdown}>Account</p>
+          {this.state.showAccountDropdown && <Account />}
+        </div>
       </div>
     );
   }

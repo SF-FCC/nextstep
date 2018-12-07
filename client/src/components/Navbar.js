ec51813eb00f3df6d0c0fffc2e7b123b3caec813
Nate Lipp <Nateplipp@gmail.com>
2018-12-07 13:08:53 -0800

    add browserrouter & prop-types lib & acc dropdown
    * feature/react-routing-routes
    
    * feature/dashboard-account-dropdown
    
    * create account form and routing, add prop-types library
    
    * Add Switch component wrapping main routes
    
    this ensures only one of the child components will render

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Account from './Account';
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showAccountDropdown: false,
+    }
+    this.toggleAccountDropdown = this.toggleAccountDropdown.bind(this);
+  }
+  toggleAccountDropdown() {
+    this.setState({ showAccountDropdown: !this.state.showAccountDropdown });
+  }
   render() {
     return (
       <div>
         <h1>NextStep</h1>
-        <a href="">Dashboard</a>
-        <a href="">Tracker</a>
-        <a href="">Account</a>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/tracker">Tracker</Link>
+        <div>
+          <p onClick={this.toggleAccountDropdown}>Account</p>
+          {this.state.showAccountDropdown && 
+            <Account hideDropdown={this.toggleAccountDropdown} />}
+        </div>
       </div>
     );
   }

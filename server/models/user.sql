8b1a567c4052e584d6b3b54d8c952a78d90549be
Daniel Escobedo <demiacle@gmail.com>
2018-12-09 09:50:29 -0800

    Add get/jobs post/jobs routes & update models (#30)
    - Refactor db connection into its own file
    - Add created/updated to models

--- server/models/user.sql
+++ server/models/user.sql
@@ -5,7 +5,9 @@ CREATE TABLE users(
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    created TIMESTAMP DEFAULT now(),
+    updated TIMESTAMP DEFAULT now()
 );
 
 COMMIT;

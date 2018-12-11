8ddfd1eac61ab016b09f8d73176fb5eec97fde5b
Daniel Escobedo <demiacle@gmail.com>
2018-12-11 11:10:07 -0800

    Add new job form (#34)
    * feature/account-update-form
    
    * feature/add-new-job-form add routing for new job form
    
    * feature/add-new-job-form actual form

--- client/src/components/AccountForm.js
+++ client/src/components/AccountForm.js
@@ -1,9 +1,66 @@
 import React, { Component } from 'react';
 
 class AccountForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+      passwordComfirmation: '',
+      currentPassword: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleAccountCancel = this.handleAccountCancel.bind(this);
+  }
+  handleChange(e) {
+    this.setState({[e.target.id]: e.target.value});
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('submitting...', this.state); 
+  }
+  handleAccountCancel() {
+    console.log('canceling account...');
+  }
   render() {
     return (
-      <div>form goes here</div>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h3>Email & Password</h3>
+          <label>*Email
+            <input 
+              id='email'
+              onChange={this.handleChange}
+              value={this.state.email} />
+          </label>
+          <label>Password
+            <input 
+              id='password'
+              type='password'
+              onChange={this.handleChange}
+              value={this.state.password} />
+              <p>Leave it blank if you dont want to change it</p>
+          </label>
+          <label>Password Comfirmation
+            <input 
+              id='passwordComfirmation'
+              type='password'
+              onChange={this.handleChange}
+              value={this.state.passwordComfirmation} />
+          </label>
+          <label>Password Comfirmation
+            <input 
+              id='currentPassword'
+              type='password'
+              onChange={this.handleChange}
+              value={this.state.currentPassword} />
+              <p>We need your current password to comfirm your changes</p>
+          </label>
+          <button>Save</button>
+        </form>
+        <p onClick={this.handleAccountCancel}>Cancel my account</p>
+      </div>
     )
   }
 }

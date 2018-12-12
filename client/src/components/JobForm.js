f59c991d3ce3503ef3aeb51d0c792bed56e60d42
Nate Lipp <Nateplipp@gmail.com>
2018-12-12 12:18:11 -0800

    add job form modal & job form styling
    * create modal for job form
    
    * load Roboto 100,400,700 from google fonts
    
    * job form styling

--- client/src/components/JobForm.js
+++ client/src/components/JobForm.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { sources } from '../data';
-import './JobForm.css';
+import styles from './JobForm.module.css';
 
 const SourceOptionsList = ({ companyNames, handleClick }) => {
   if (companyNames) {
@@ -9,9 +9,11 @@ const SourceOptionsList = ({ companyNames, handleClick }) => {
       <ul>
         {companyNames.map(companyArr => {
           return (
-            <li key={companyArr[1]} 
+            <li className={styles.companyAutoPopulate}
+                key={companyArr[1]} 
                 onClick={handleClick.bind(this, companyArr)}>
-              <span>{companyArr[1] + ' - '}</span>
+              <span>{companyArr[1] + ' - '}
+              </span>
               <span>{companyArr[0]}</span>
             </li>
           )
@@ -27,7 +29,7 @@ const SourceOptionsList = ({ companyNames, handleClick }) => {
 }
 
 const DisplaySource = ({ sourceSymbol, source, reset }) => (
-  <div onClick={reset}>
+  <div className={styles.companyName} onClick={reset}>
     <span>{sourceSymbol + ' - '}</span>
     <span>{source}</span>
   </div>
@@ -51,6 +53,7 @@ class JobForm extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSourceOptionSelect = this.handleSourceOptionSelect.bind(this);
     this.resetSourceOptionSelect = this.resetSourceOptionSelect.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
   }
   handleSourceOptionSelect(companyName) {
     this.setState({sourceSymbol: companyName[1]});
@@ -80,6 +83,9 @@ class JobForm extends Component {
     });
     this.props.hideForm();
   }
+  toggleForm(e) {
+    e.target.id === 'hide' && this.props.hideForm();
+  }
   handleChange(e) {
     if (e.target.id === 'source') {
       this.setState({sourceOptions: sources[e.target.value.toLowerCase()]});
@@ -88,68 +94,79 @@ class JobForm extends Component {
   }
   render() {
     return (
-      <div>
-        <h1>Add New Job</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>Job Posting URL
-            <input 
-              id="jobPostingUrl"
-              onChange={this.handleChange}
-              placeholder="Search Companies..."
-              value={this.state.jobPostingUrl} /></label>
-          <label>Company
-            <input 
-              id="company"
-              onChange={this.handleChange}
-              value={this.state.company} /></label>
-          <label>Job Title
-            <input 
-              id="jobTitle"
-              onChange={this.handleChange}
-              value={this.state.jobTitle} /></label>
-          <label>Status
-            <select 
-              id="status"
-              value={this.state.status}
-              onChange={this.handleChange}>
-              <option value="interested">Interested</option>
-              <option value="applied">Applied</option>
-              <option value="phoneCall">Phone Call</option>
-              <option value="assignment">Assignment</option>
-              <option value="interview">Interview</option>
-              <option value="offer">Offer</option>
-              <option value="accepted">Accepted</option>
-              <option value="withdrawn">Withdrawn</option>
-              <option value="expired">Expired</option>
-              <option value="notAFit">Not A Fit</option>
-            </select>
-          </label>
-          <label>Location
-            <input 
-              id="location"
-              onChange={this.handleChange}
-              value={this.state.location} /></label>
-          <label>Source
-            {this.state.displaySourceForm 
-              ? 
-                <div>
-                  <input 
-                    id="source"
-                    placeholder="Where did you find this job?"
-                    onChange={this.handleChange}
-                    value={this.state.source} />
-                    <SourceOptionsList
-                      handleClick={this.handleSourceOptionSelect}
-                      companyNames={this.state.sourceOptions} />
-                </div>
-              : 
-                <DisplaySource 
-                  reset={this.resetSourceOptionSelect}
-                  sourceSymbol={this.state.sourceSymbol}
-                  source={this.state.source} />}
-          </label>
-          <button>submit</button>
-        </form>
+      <div 
+        id="hide"
+        onClick={this.toggleForm}
+        className={styles.formOuterContainer}>
+        <div className={styles.formInnerContainer}>
+          <div className={styles.formHeader}>
+            <h1 className={styles.h1}>Add New Job</h1>
+            <span
+              id="hide"
+              className={"closeForm"}
+              onClick={this.toggleForm}>X
+            </span>
+          </div>
+          <form onSubmit={this.handleSubmit}>
+            <label>Job Posting URL
+              <input 
+                id="jobPostingUrl"
+                onChange={this.handleChange}
+                value={this.state.jobPostingUrl} /></label>
+            <label>Company
+              <input 
+                id="company"
+                onChange={this.handleChange}
+                value={this.state.company} /></label>
+            <label>Job Title
+              <input 
+                id="jobTitle"
+                onChange={this.handleChange}
+                value={this.state.jobTitle} /></label>
+            <label>Status
+              <select 
+                id="status"
+                value={this.state.status}
+                onChange={this.handleChange}>
+                <option value="interested">Interested</option>
+                <option value="applied">Applied</option>
+                <option value="phoneCall">Phone Call</option>
+                <option value="assignment">Assignment</option>
+                <option value="interview">Interview</option>
+                <option value="offer">Offer</option>
+                <option value="accepted">Accepted</option>
+                <option value="withdrawn">Withdrawn</option>
+                <option value="expired">Expired</option>
+                <option value="notAFit">Not A Fit</option>
+              </select>
+            </label>
+            <label>Location
+              <input 
+                id="location"
+                onChange={this.handleChange}
+                value={this.state.location} /></label>
+            <label>Source
+              {this.state.displaySourceForm 
+                ? 
+                  <div>
+                    <input 
+                      id="source"
+                      placeholder="Where did you find this job?"
+                      onChange={this.handleChange}
+                      value={this.state.source} />
+                      <SourceOptionsList
+                        handleClick={this.handleSourceOptionSelect}
+                        companyNames={this.state.sourceOptions} />
+                  </div>
+                : 
+                  <DisplaySource 
+                    reset={this.resetSourceOptionSelect}
+                    sourceSymbol={this.state.sourceSymbol}
+                    source={this.state.source} />}
+            </label>
+            <button>SAVE</button>
+          </form>
+        </div>
       </div>
     )
   }

9d5d47d5f7b17506cf71183afe5278028469e9ef
demiacle <demiacle@gmail.com>
2018-12-12 00:12:55 -0800

    add graceful shutdown
    also change quotes to single

--- server/db/postgres.js
+++ server/db/postgres.js
@@ -1,16 +1,23 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.PG_USER || "postgres",
-  host: process.env.PG_HOST || "127.0.0.1",
-  database: process.env.PG_DB || "postgres",
-  password: process.env.PG_PW || "postgres",
+  user: process.env.PG_USER || 'postgres',
+  host: process.env.PG_HOST || '127.0.0.1',
+  database: process.env.PG_DB || 'postgres',
+  password: process.env.PG_PW || 'postgres',
   port: process.env.PG_PORT || 5432
+});
+
+// the pool with emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on postgress pool', err);
+  // process.exit(-1);
 });
 
 // Hello world!
 async function helloWorld() {
-  const res = await pool.query("SELECT current_database()");
+  const res = await pool.query('SELECT current_database()');
   console.log(res.rows[0]);
 }
 
--- server/index.js
+++ server/index.js
@@ -1,22 +1,34 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+require('dotenv').config();
 const app = express();
+const pool = require('./db/postgres').pool;
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 // TODO change static location and serve with nginx
-app.use(express.static("../client/build"));
+app.use(express.static('../client/build'));
 
 // Routes
-require("./routes/index")(app);
-require("./routes/jobs")(app);
+require('./routes/index')(app);
+require('./routes/jobs')(app);
 
 //Server Setup
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Serving starting on ${PORT}`);
 });
+
+// Graceful shutdown
+function gracefulShutdown() {
+  console.log('Closing http server.');
+  server.close(() => {
+    console.log('Closing db');
+    pool.end(err => console.log(err));
+  });
+}
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGINT', gracefulShutdown);

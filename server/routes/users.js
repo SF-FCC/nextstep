5d72e4c95dc61ddae537a8d73c0a1677214ceed2
Christian <cazenaty@yahoo.com>
2018-12-12 23:09:22 -0800

    Update auth routes and responses (#59)
    * Require users route
    
    * Add bcrypt
    
    * Save hashed pw to DB
    
    * Adding auth routes
    
    * Adding initial login routing
    
    * Temp logout resonse
    
    * Remove old register route handling

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,26 +1,9 @@
 const pool = require("../db/postgres").pool;
 
 module.exports = app => {
-  app.post("/users", async (req, res, next) => {
-    const { first_name, last_name, email, password } = req.body;
-    // TODO Validate password
-    // TODO crypto password
-    const insert = `
-      INSERT INTO users (
-        first_name,
-        last_name,
-        email,
-        password
-        )
-      VALUES ($1, $2, $3, $4);`;
-    try {
-      const user = await pool.query(insert, [first_name, last_name, email, password]);
-      return res.json(user);
-    } catch (err) {
-      // TODO add error message for email already taken
-      next(err);
-    }
-  });
+  // Use for user profile update form
+  app.put("/users", (req, res) => {});
+
   app.get("/users", async (req, res, next) => {
     const query = `SELECT * from users`;
     try {

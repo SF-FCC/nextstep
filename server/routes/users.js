8133d5436e9326b7e0b04ecde3d414cb5d6c56f6
Christian Zenaty <cazenaty@yahoo.com>
2018-12-12 16:22:55 -0800

    Save hashed pw to DB

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,19 @@
 const pool = require("../db/postgres").pool;
+const bcrypt = require("bcrypt");
 
 module.exports = app => {
+  // TEMP REGISTER ROUTE - Move to auth routes
   app.post("/users", async (req, res, next) => {
     const { first_name, last_name, email, password } = req.body;
-    // TODO Validate password
-    // TODO crypto password
+    // TODO Validate password/ email
+    const saltRounds = 10;
+    const pwHash = bcrypt.hashSync(password, saltRounds, (err, hash) => {
+      if (err) {
+        return console.error("bcrypt hashing error", err);
+      }
+      return hash;
+    });    
+
     const insert = `
       INSERT INTO users (
         first_name,
@@ -14,13 +23,14 @@ module.exports = app => {
         )
       VALUES ($1, $2, $3, $4);`;
     try {
-      const user = await pool.query(insert, [first_name, last_name, email, password]);
+      const user = await pool.query(insert, [first_name, last_name, email, pwHash]);
       return res.json(user);
     } catch (err) {
       // TODO add error message for email already taken
       next(err);
     }
   });
+
   app.get("/users", async (req, res, next) => {
     const query = `SELECT * from users`;
     try {

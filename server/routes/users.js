dad494df0cdc826f59c25f2eb1c38f3af7d40f7f
Christian Zenaty <cazenaty@yahoo.com>
2018-12-12 18:20:58 -0800

    Remove old register route handling

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,35 +1,8 @@
 const pool = require("../db/postgres").pool;
-const bcrypt = require("bcrypt");
 
 module.exports = app => {
-  // TEMP REGISTER ROUTE - Move to auth routes
-  app.post("/users", async (req, res, next) => {
-    const { first_name, last_name, email, password } = req.body;
-    // TODO Validate password/ email
-    const saltRounds = 10;
-    const pwHash = bcrypt.hashSync(password, saltRounds, (err, hash) => {
-      if (err) {
-        return console.error("bcrypt hashing error", err);
-      }
-      return hash;
-    });    
-
-    const insert = `
-      INSERT INTO users (
-        first_name,
-        last_name,
-        email,
-        password
-        )
-      VALUES ($1, $2, $3, $4);`;
-    try {
-      const user = await pool.query(insert, [first_name, last_name, email, pwHash]);
-      return res.json(user);
-    } catch (err) {
-      // TODO add error message for email already taken
-      next(err);
-    }
-  });
+  // Use for user profile update form
+  app.put("/users", (req, res) => {});
 
   app.get("/users", async (req, res, next) => {
     const query = `SELECT * from users`;

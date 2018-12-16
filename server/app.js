b3973547d95f3d0373b527b864db6689d1d6019c
demiacle <demiacle@gmail.com>
2018-12-15 19:09:31 -0800

    Refactor server out of express app

--- server/app.js
+++ server/app.js
@@ -24,19 +24,11 @@ require("./routes/jobs")(app);
 require("./routes/users")(app);
 require("./routes/auth")(app);
 
-//Server Setup
-const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => {
-  console.log(`Serving starting on ${PORT}`);
-});
-
-// Graceful shutdown
 function gracefulShutdown() {
-  console.log("Closing http server.");
-  server.close(() => {
-    console.log("Closing db");
-    pool.end(err => console.log(err));
-  });
+  console.log("Closing db");
+  pool.end(err => console.log(err));
 }
 process.on("SIGTERM", gracefulShutdown);
 process.on("SIGINT", gracefulShutdown);
+
+module.exports.express = app;

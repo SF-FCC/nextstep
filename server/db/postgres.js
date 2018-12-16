0a866e0ec93290234750b61536f4a9344581887b
demiacle <demiacle@gmail.com>
2018-12-15 19:01:21 -0800

    Clean up db file

--- server/db/postgres.js
+++ server/db/postgres.js
@@ -1,30 +1,34 @@
-const { Pool } = require('pg');
-const fs = require('fs');
+const { Pool } = require("pg");
+const fs = require("fs");
 
-const createUsersTable = fs.readFileSync('./models/users.sql').toString();
-const createJobsTable = fs.readFileSync('./models/jobs.sql').toString();
+// Load table schemas
+const createUsersTable = fs.readFileSync("./models/users.sql").toString();
+const createJobsTable = fs.readFileSync("./models/jobs.sql").toString();
 
+// Config postgres connection
 const pool = new Pool({
-  user: process.env.PG_USER || 'postgres',
-  host: process.env.PG_HOST || '127.0.0.1',
-  database: process.env.PG_DB || 'postgres',
-  password: process.env.PG_PW || 'postgres',
+  user: process.env.PG_USER || "postgres",
+  host: process.env.PG_HOST || "127.0.0.1",
+  database: process.env.PG_DB || "postgres",
+  password: process.env.PG_PW || "postgres",
   port: process.env.PG_PORT || 5432
 });
 
-// the pool with emit an error on behalf of any idle clients
+// The pool wil emit an error on behalf of any idle clients
 // it contains if a backend error or network partition happens
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on postgress pool', err);
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on postgress pool", err);
   // process.exit(-1);
 });
 
-// Hello world!
-async function helloWorld() {
+/**
+ * Create required tables in the db if they don't exist
+ */
+async function initializeTables() {
   try {
-    const res = await pool.query('SELECT current_database()');
+    const res = await pool.query("SELECT current_database()");
     console.log(res.rows[0]);
-    // TODO log table creation
+    // TODO log any table creation
     await pool.query(createUsersTable);
     await pool.query(createJobsTable);
   } catch (err) {
@@ -32,6 +36,6 @@ async function helloWorld() {
   }
 }
 
-helloWorld();
+initializeTables();
 
 module.exports.pool = pool;

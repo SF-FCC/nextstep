8c73a43e9dcdfa727b5790cd1fcb8344694f35fa
Nalipp <nateplipp@gmail.com>
2018-12-18 15:31:00 -0800

    fix req.body name missmatch

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -4,26 +4,25 @@ const pool = require("../db/postgres").pool;
 module.exports = app => {
   app.post("/jobs", async (req, res, next) => {
     // TODO - Need to add user_id from client req (action creator)
-
-    const posting_url = req.body.jobPostingUrl;
-    const company_name = req.body.company;
-    const company_url = req.body.companyUrl;
-    const job_title = req.body.jobTitle;
-    const current_status = req.body.current_status;
-    const job_location = req.body.location;
-    const user_id = null;
+    const { posting_url,
+    company_name,
+    company_url,
+    job_title,
+    current_status,
+    job_location,
+    user_id } = req.body
 
     const insert = `
       INSERT INTO jobs (
         posting_url,
         company_name,
-        company_url,
         job_title,
         current_status,
         job_location,
         user_id
         )
-      VALUES ($1, $2, $3, $4, $5, $6, $7);`;
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING *;`;
     try {
       const jobs = await pool.query(insert, [
         posting_url,

cad6ebe6155e0e0286b231d515b90d8c1869d19e
Nalipp <nateplipp@gmail.com>
2018-12-19 15:38:05 -0800

    add sorting functionality to tracker component

--- client/src/components/Tracker.js
+++ client/src/components/Tracker.js
@@ -9,31 +9,68 @@ import styles from "./Tracker.module.css";
  * Has a searchbar
  *
  * TODO - when a job status is changed to applied there should be a form for collecting the date
+ *
+ * TODO - when the job status form is complete, change the <td>DATE APPLIED</td> id to the name used in the db to store the created on date (right now it is sorting by the date of the 'created' field
  */
 
-const TrackerTable = ({ jobApps }) => (
-  <table className={styles.tracker__table}>
-    <tbody className={styles.tracker__table_heading}>
-      <tr>
-        <td>COMPANY</td>
-        <td>JOB TITLE</td>
-        <td>STATUS</td>
-        <td className={"mobile_hide"}>LOCATION</td>
-        <td className={"mobile_hide"}>DATA APPLIED</td>
-      </tr>
-    </tbody>
-    <tbody className={styles.tracker__table_body}>
-      {jobApps.map(jobApp => (
-        <tr key={jobApp.id}>
-          <td>{jobApp.company_name}</td>
-          <td>{jobApp.job_title}</td>
-          <td>{jobApp.current_status}</td>
-          <td className={"mobile_hide"}>{jobApp.job_location}</td>
-          <td className={"mobile_hide"}>{null}</td>
-        </tr>))}
-    </tbody>
-  </table>
-)
+class TrackerTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSortBy: null, 
+      jobApps: props.jobApps,
+    }
+    this.sortTableBy = this.sortTableBy.bind(this);
+    this.handleSortRequest = this.handleSortRequest.bind(this);
+  }
+  sortTableBy(sortType) {
+    let jobsCopy = this.state.jobApps.slice();
+
+    if (this.state.currentSortBy === sortType) {
+      this.setState({
+        currentSortBy: null,
+        jobApps: jobsCopy.reverse(),
+      });
+    } else {
+      let res = jobsCopy.sort((acc, job) => {
+        return acc[sortType].toLowerCase() > job[sortType].toLowerCase();
+      });
+
+      this.setState({
+        currentSortBy: sortType,
+        jobApps: res
+      });
+    }
+  }
+  handleSortRequest(e) {
+    if (e.target.id) this.sortTableBy(e.target.id);
+  }
+  render() {
+    return (
+      <table className={styles.tracker__table}>
+        <tbody className={styles.tracker__table_heading}>
+          <tr onClick={this.handleSortRequest}>
+            <td id='company_name'>COMPANY</td>
+            <td id='job_title'>JOB TITLE</td>
+            <td id='current_status'>STATUS</td>
+            <td id='job_location' className={"mobile_hide"}>LOCATION</td>
+            <td id='created' className={"mobile_hide"}>DATE APPLIED</td>
+          </tr>
+        </tbody>
+        <tbody className={styles.tracker__table_body}>
+          {this.state.jobApps.map(jobApp => (
+            <tr key={jobApp.id}>
+              <td>{jobApp.company_name}</td>
+              <td>{jobApp.job_title}</td>
+              <td>{jobApp.current_status}</td>
+              <td className={"mobile_hide"}>{jobApp.job_location}</td>
+              <td className={"mobile_hide"}>{jobApp.created}</td>
+            </tr>))}
+        </tbody>
+      </table>
+    )
+  }
+}
 
 class Tracker extends Component {
   constructor(props) {

0c23cc67ad7c18c3a291e1bbc095a8adfb2c57e4
demiacle <demiacle@gmail.com>
2018-12-26 02:37:09 -0800

    Add splash page scaffold

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
     return (
       <div>
         <Navbar location={this.props.location} />
-        <div style={{ padding: "20px 5px 20px 20px" }}>
+        <div style={{ padding: "20px" }}>
           {!this.props.isLoggedIn ? (
             <Switch>
               <Route exact path="/" component={SplashPage} />

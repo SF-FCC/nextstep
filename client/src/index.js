b192709a86f47815dde9cdb30a56a003ed08a64f
Christian Zenaty <cazenaty@yahoo.com>
2018-12-27 20:07:58 -0800

    Create browserhistory and add to Router props

--- client/src/index.js
+++ client/src/index.js
@@ -3,24 +3,22 @@ import ReactDOM from "react-dom";
 import "./styleReset.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 import reduxThunk from "redux-thunk";
+import history from "./utilities/history";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  reducers,
-  /* preloadedState, */ composeEnhancers(applyMiddleware(reduxThunk))
-);
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <Router history={history}>
       <Route render={props => <App location={props.location} />} />
-    </BrowserRouter>
+    </Router>
   </Provider>,
   document.getElementById("root")
 );

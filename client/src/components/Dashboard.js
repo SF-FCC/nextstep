f5030ed1ee2d4b83de332a54c11373a547d7489c
Tiffany Lam <tlam5@sbcglobal.net>
2018-12-28 18:49:33 -0800

    Connect dashboard cards to jobAppDetail component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import JobForm from "./JobForm";
+import JobAppDetail from "./JobAppDetail";
 import { connect } from "react-redux";
-import { showJobForm, getAllJobApps } from "../actions";
+import { setCurrentJobApp, showJobDetail, showJobForm, getAllJobApps } from "../actions";
 import { NavLink } from "react-router-dom";
 import styles from "./Dashboard.module.css";
 
 // TODO: change updated to minutes once 60s threshold reached (basic formatting)
 
-const JobItem = ({ job }) => {
+const JobItem = ({ job, handleShowJobAppDetail }) => {
   return (
-    <li key={job.id} className={styles.card}>
+    <li className={styles.card} onClick={handleShowJobAppDetail}>
       <div className={styles.card_top}>
         <span>
           <div className={styles.card_top__company}>{job.company_name}</div>
@@ -36,6 +37,10 @@ class Dashboard extends Component {
   componentDidMount() {
     this.props.getAllJobApps();
   }
+  handleShowJobAppDetail(jobApp) {
+    this.props.setCurrentJobApp(jobApp);
+    this.props.showJobDetail();
+  }
   handleShowJobForm() {
     this.props.showJobForm();
   }
@@ -59,11 +64,17 @@ class Dashboard extends Component {
                 <span className={styles.bold}>+</span> Add Job
               </li>
               {this.props.allJobApps.map(job => (
-                <JobItem key={job.id} job={job} />
+                <JobItem 
+                  key={job.id} 
+                  job={job}
+                  handleShowJobAppDetail={this.handleShowJobAppDetail.bind(this, job)}
+                />
               ))}
             </ul>
           </div>
           {this.props.isShowingJobForm && <JobForm />}
+          {this.props.isShowingJobDetail
+            && <JobAppDetail currentJobApp={this.props.currentJobApp} />}
         </div>
       </div>
     );
@@ -78,12 +89,16 @@ Dashboard.defaultProps = {
 
 const mapStateToProps = state => {
   return {
+    isShowingJobDetail: state.toggleDisplays.isShowingJobDetail,
     isShowingJobForm: state.toggleDisplays.isShowingJobForm,
+    currentJobApp: state.jobApp.currentJobApp,
     allJobApps: state.jobApp.jobApps
   };
 };
 
 const mapDispatchToProps = dispatch => ({
+  setCurrentJobApp: (job) => dispatch(setCurrentJobApp(job)),
+  showJobDetail: () => dispatch(showJobDetail()),
   showJobForm: () => dispatch(showJobForm()),
   getAllJobApps: () => dispatch(getAllJobApps())
 });

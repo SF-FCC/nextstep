33874de2593ab32b7e31d0be3f0f97d3a5b569e8
Nate Lipp <Nateplipp@gmail.com>
2018-12-31 15:39:26 -0800

    Add style to search form & fix dropdown (#136)
    * search-bar-styling
    
    * company search functionality

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -3,13 +3,14 @@ import getCompanyDictionary from '../data/getCompanyDictionary';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import CompaniesDropdown from './CompaniesDropdown';
+import styles from './SearchForm.module.css';
 
 class SearchForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       value: '',
-      dropDownValues: null,
+      dropDownValues: [],
       companyDictionary: {}
     }
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -18,20 +19,29 @@ class SearchForm extends Component {
   handleInputChange(e) {
     this.setState({ value: e.target.value }, () => {
       this.setState({ companyDictionary: getCompanyDictionary(this.props.jobApps) }, () => {
-        this.setState({ dropDownValues: this.state.companyDictionary[this.state.value] })
+        if (this.state.value === '') {
+          this.setState({ dropDownValues: [] });
+        } else {
+          let results = this.state.companyDictionary[this.state.value];
+          this.setState({ dropDownValues: results || ['no results']})
+        }
       })
     });
   }
   resetDropdown() {
     this.setState({ 
       value: '',
-      dropDownValues: null,
+      dropDownValues: [],
     });
   }
   render() {
     return (
       <form>
-        <input onChange={this.handleInputChange} value={this.state.value} />
+        <input
+          className={styles.input}
+          onChange={this.handleInputChange} 
+          placeholder="company search"
+          value={this.state.value} />
         {this.state.dropDownValues && 
           <CompaniesDropdown 
             resetDropdown={this.resetDropdown}

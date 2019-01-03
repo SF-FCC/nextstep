f7e7bb64fde8348fc9b4b79d7605b43dadbe3eb9
demiacle <demiacle@gmail.com>
2019-01-02 20:55:44 -0800

    Add splash page styles

--- client/src/components/SplashPage.js
+++ client/src/components/SplashPage.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import styles from "./SplashPage.module.css";
+import g_styles from "../globals.module.css";
+import { NavLink } from "react-router-dom";
 
 /**
  * The splash page a user lands on when not logged in
@@ -8,10 +10,13 @@ export default class SplashPage extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <p className={styles.value_prop}>
-          Track your stuff and be awesome! Aslo some other thing that sounds cool.
-        </p>
+        <h1 className={styles.value_prop}>
+          Manage all your opportunities in one central location.
+        </h1>
         <div className={styles.cool_image} />
+        <NavLink className={g_styles.primary_button + " " + styles.login_button} to="/login">
+          Try it for free
+        </NavLink>
       </div>
     );
   }

2f5a234999698b48e65ad97f95f937d97b141e8d
Nate Lipp <Nateplipp@gmail.com>
2019-01-10 16:47:57 -0800

    User email and password update functionality
    * populate user email in account form
    
    * user account password update functionality

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -55,7 +55,6 @@ export const requestLogin = (email, password) => {
       axios
         .post(url, body)
         .then(r => {
-          console.log(r.data);
           // (name, token, maxage) max age set at 30 minutes
           setItem("token", r.data.token, 1800);
           // Update store
@@ -125,7 +124,7 @@ export const updateEmail = (email, newEmail, password) => async dispatch => {
   const validPassword = await axios.post("/auth/login", { email, password });
 
   if (validPassword.status === 200) {
-    const response = await axios.put("/users", body, authHeaders);
+    const response = await axios.put("/users/email", body, authHeaders);
     if (response.status === 200) {
       dispatch({ type: "UPDATE_EMAIL", payload: newEmail });
       history.push("/tracker");
@@ -135,15 +134,18 @@ export const updateEmail = (email, newEmail, password) => async dispatch => {
   }
 };
 
-/**
- * Unused
- * @param {*} password
- */
-export const updatePassword = password => {
-  return {
-    type: "UPDATE_PASSWORD",
-    payload: password
-  };
+export const updatePassword = (email, newPw, password) => async dispatch => {
+  const body = { email, newPw };
+  const authHeaders = await reqConfig("token");
+  const validPassword = await axios.post("/auth/login", { email, password });
+  if (validPassword.status === 200) {
+    const response = await axios.put("/users/password", body, authHeaders);
+    if (response.status === 200) {
+      history.push("/tracker");
+    } else {
+      dispatch({ type: "UPDATE_PW_ERR", message: "unable to update password" });
+    }
+  }
 };
 
 /**
